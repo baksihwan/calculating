acd113512ad3441d4395a5dee10a09e5cea0a226
박시환 <baksh999@naver.com>
2024-10-17 10:38:27 +0900

    If문을 이용한 계산기 구현

--- src/Cal/LV1/LV1if.java
+++ src/Cal/LV1/LV1if.java
@@ -1,32 +1,32 @@
-package Cal.LV1;
-
-import java.util.Scanner;
-
-public class LV1if {
-    public static void main(String[] args) {
-        int one = 0;
-        int two = 0;
-
-        String basic = "";
-        Scanner sc = new Scanner(System.in);
-        System.out.println("첫번째 값을 입력해주세요 : ");
-        one = sc.nextInt();
-
-        System.out.println("두번째 값을 입력해주세요. : ");
-        two = sc.nextInt();
-        System.out.println("사칙연산부호를 선택하세요(+,-,*,/) : ");
-        basic = sc.next();
-        if(basic.equals("+")){
-            System.out.println(one+"+"+two+"="+(one+two));
-        }
-        else if(basic.equals("-")){
-            System.out.println(one+"-"+two+"="+(one-two));
-        }else if(basic.equals("*")){
-            System.out.println(one+"*"+two+"="+(one*two));
-        }else if(basic.equals("/")){
-            System.out.println(one+"/"+two+"="+(one/two));
-        }else{
-            System.out.println("불분명한 연산자입니다.");
-        }
-    }
-}
+//package Cal.LV1;
+//
+//import java.util.Scanner;
+//
+//public class LV1if {
+//    public static void main(String[] args) {
+//        int one = 0;
+//        int two = 0;
+//
+//        String basic = "";
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("첫번째 값을 입력해주세요 : ");
+//        one = sc.nextInt();
+//
+//        System.out.println("두번째 값을 입력해주세요. : ");
+//        two = sc.nextInt();
+//        System.out.println("사칙연산부호를 선택하세요(+,-,*,/) : ");
+//        basic = sc.next();
+//        if(basic.equals("+")){
+//            System.out.println(one+"+"+two+"="+(one+two));
+//        }
+//        else if(basic.equals("-")){
+//            System.out.println(one+"-"+two+"="+(one-two));
+//        }else if(basic.equals("*")){
+//            System.out.println(one+"*"+two+"="+(one*two));
+//        }else if(basic.equals("/")){
+//            System.out.println(one+"/"+two+"="+(one/two));
+//        }else{
+//            System.out.println("불분명한 연산자입니다.");
+//        }
+//    }
+//}
