d381e737f10147221f3d4c58c6e5a061891f772c
박시환 <baksh999@naver.com>
2024-10-17 10:33:12 +0900

    계산기 구현

---
+++ src/Cal/Lv1Calculator.java
@@ -0,0 +1,46 @@
+package Cal;
+
+import java.util.Scanner;
+
+public class Lv1Calculator {
+    public static void main(String[] args) {
+
+
+//        Scanner sc = new Scanner(System.in); //1. 덧셈 계산기
+//        System.out.println("첫번째 숫자를 입력해주세요: ");
+//        double num1 = Double.parseDouble(sc.nextLine());
+//
+//        System.out.println("두번째 숫자를 입력해주세요.");
+//        double num2 = Double.parseDouble(sc.nextLine());
+//        System.out.println(num1 + num2);
+//    }
+
+//        Scanner sc = new Scanner(System.in); //2. 뺄셈 계산기
+//        System.out.println("첫번째 숫자를 입력해주세요: ");
+//        double num1 = Double.parseDouble(sc.nextLine());
+//
+//        System.out.println("두번째 숫자를 입력해주세요.");
+//        double num2 = Double.parseDouble(sc.nextLine());
+//        System.out.println(num1 - num2);
+//    }
+
+//        Scanner sc = new Scanner(System.in); //3. 곱셈 계산기
+//        System.out.println("첫번째 숫자를 입력해주세요: ");
+//        double num1 = Double.parseDouble(sc.nextLine());
+//
+//        System.out.println("두번째 숫자를 입력해주세요.");
+//        double num2 = Double.parseDouble(sc.nextLine());
+//        System.out.println(num1 * num2);
+//    }
+
+//        Scanner sc = new Scanner(System.in); //4. 나눗셈 계산기
+//        System.out.println("첫번째 숫자를 입력해주세요: ");
+//        double num1 = Double.parseDouble(sc.nextLine());
+//
+//        System.out.println("두번째 숫자를 입력해주세요.");
+//        double num2 = Double.parseDouble(sc.nextLine());
+//        System.out.println(num1 / num2);
+//    }
+//    }
+   }
+}
